dcb03fd478f8c9140bc3a2604dc57c615339b4cc
JadeLee <dahyunlee9999@gmail.com>
2026-09-15 13:43:35 +0900

    Scenario completed

--- shop.py
+++ shop.py
@@ -10,7 +10,7 @@ class Customer:
         return f"[{self.grade}]{self.name}(포인트: {self.points:,})"
 class Order:
     def add_item(self,name,price):
-        self.item.append((name,price))
+        self.items.append((name,price))
     def total_price(self):
         raw_total=0
         for item in self.items:
@@ -22,3 +22,34 @@ class Order:
         final_price=self.total_price()
         self.customer.add_points(final_price)
         return final_price
+vip_user=Customer()
+vip_user.name="김철수"
+vip_user.grade="vip"
+vip_user.points=0
+basic_user=Customer()
+basic_user.name="김영희"
+basic_user.grade="basic"
+basic_user.points=0
+order1=Order()
+order1.order_id="Order1"
+order1.customer=vip_user
+order1.items=[]
+order1.add_item("초코케이크",50000)
+order1.add_item("아메리카노",5000)
+order2=Order()
+order2.order_id="Order2"
+order2.customer=basic_user
+order2.items=[]
+order2.add_item("딸기케이크", 30000)
+order3=Order()
+order3.order_id="Order3"
+order3.customer=vip_user
+order3.items=[]
+order3.add_item("말차라떼", 8000)
+print("=== 주문 결제 내역 ===")
+print(f"주문 1 결제 금액: {order1.pay():,}원")
+print(f"주문 2 결제 금액: {order2.pay():,}원")
+print(f"주문 3 결제 금액: {order3.pay():,}원")
+print("\n=== 최종 고객 상태 ===")
+print(vip_user.summary())
+print(basic_user.summary())
